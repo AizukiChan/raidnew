fe2a43b9a782e45ec357b870ddec8a3fe54faee7
KhadafiaRani <131012758+AizukiChan@users.noreply.github.com>
2024-05-30 03:38:32 +0900

    Delete commands/afk.js

--- commands/afk.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'afk',
-    description: 'Atur status AFK Anda.',
-    execute(message, args, afkUsers) {
-        const afkMessage = args.join(' ') || 'AFK';
-        afkUsers.set(message.author.id, { message: afkMessage });
-        message.reply(`Anda sekarang AFK: ${afkMessage}`);
-    }
-};
